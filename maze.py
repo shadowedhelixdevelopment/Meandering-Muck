c3aa971f0ca23ff71a76bc38d1f7930a5f0b311c
Shadowedvaca <shadowedvaca@gmail.com>
2019-03-01 20:57:57 -0700

    First pass at doors

--- maze.py
+++ maze.py
@@ -18,30 +18,73 @@ def make_maze():
     return maze
 
 
-def maze_section(maze, minx, maxx, miny, maxy, iteration=0, grid=0):
-    wallx = rand(minx, maxx)
-    wally = rand(miny, maxy)
-    # maze[(minx - minspace), wally] = maze[(maxx + minspace - 1), wally] = 1
-    # maze[wallx, (miny - minspace)] = maze[wallx, (maxy + minspace - 1)] = 1
-    maze[(minx - minspace):(maxx + minspace), wally] = 1
-    maze[wallx, (miny - minspace):(maxy + minspace)] = 1
+def maze_section(maze, minx, maxx, miny, maxy):
+    skip = 0
+    # if match, set wall, otherwise make it random
+    if minx == maxx:
+        wallx = minx
+    else:
+        wallx = rand(minx, maxx)
+    if miny == maxy:
+        wally = miny
+    else:
+        wally = rand(miny, maxy)
+    # set bits to 1 for the walls
+    maze[wally, (minx - minspace):(maxx + minspace)] = 1
+    maze[(miny - minspace):(maxy + minspace), wallx] = 1
+    # Make 3 doors at random
+    # coin flip to see if a door should be placed, otherwise flag a skip
+    if rand(1, 3) == 1:
+        maze = door(maze, minx, wallx, wally, "y")  # N
+        print("North Door Made")
+    else:
+        skip = 1
+    if rand(1, 3) == 1 or skip == 1:
+        maze = door(maze, wally, maxy, wallx, "x")  # E
+        print("East Door Made")
+    else:
+        skip = 1
+    if rand(1, 3) == 1 or skip == 1:
+        maze = door(maze, wallx, maxx, wally, "y")  # S
+        print("South Door Made")
+    else:
+        skip = 1
+    if skip == 1:
+        maze = door(maze, miny, wally, wallx, "x")  # W
+        print("West Door Made")
+
     if wallx - minspace > minx and wally - minspace > miny:
-        maze = maze_section(maze, minx, (wallx - minspace - 1), miny, (wally - minspace), (iteration + 1), 1)
+        maze = maze_section(maze, minx, (wallx - minspace - 1), miny, (wally - minspace))
     if wallx + minspace < maxx and wally - minspace > miny:
-        maze = maze_section(maze, (wallx + minspace), maxx, miny, (wally - minspace), (iteration + 1), 2)
+        maze = maze_section(maze, (wallx + minspace), maxx, miny, (wally - minspace))
     if wallx - minspace > minx and wally + minspace < maxy:
-        maze = maze_section(maze, minx, (wallx - minspace), (wally + minspace), maxy, (iteration + 1), 3)
+        maze = maze_section(maze, minx, (wallx - minspace), (wally + minspace), maxy)
     if wallx + minspace < maxx and wally + minspace < maxy:
-        maze = maze_section(maze, (wallx + minspace), maxx, (wally + minspace), maxy, (iteration + 1), 4)
+        maze = maze_section(maze, (wallx + minspace), maxx, (wally + minspace), maxy)
     return maze
 
 
-width = 200
-height = 200
-minspace = 10
+def door(maze, mind, maxd, nondoor, dooraxis):
+    if (maxd - mind) <= doorwidth:
+        doormin = mind
+        doormax = maxd
+    else:
+        doormin = rand(mind, (maxd - doorwidth))
+        doormax = (doormin + doorwidth)
+    print(dooraxis, doormin, doormax, nondoor)
+    if dooraxis == "x":
+        maze[nondoor, round(doormin):round(doormax)] = 0
+    else:
+        maze[round(doormin):round(doormax), nondoor] = 0
+    return maze
+
+
+width = 15
+height = 15
+minspace = 2
+doorwidth = 1 # round((minspace / 2), 0)
 
 pyplot.figure(figsize=(10, 5))
-pyplot.imshow(make_maze(), cmap=pyplot.cm.binary, interpolation='nearest')
+pyplot.imshow(make_maze(), cmap=pyplot.cm.binary, interpolation=None)
 pyplot.xticks([]), pyplot.yticks([])
 pyplot.show()
-
