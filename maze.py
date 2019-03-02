36e658dd47208a373d62623628d524fbb58d6f23
Shadowedvaca <shadowedvaca@gmail.com>
2019-03-02 11:58:04 -0700

    Fixed wall and door code

--- maze.py
+++ maze.py
@@ -6,83 +6,70 @@ import matplotlib.pyplot as pyplot
 def make_maze():
     shape = ((height * 2) + 1, (width * 2) + 1)
     # Build Maze
-    maze = numpy.zeros(shape, dtype=bool)
+    maze = numpy.zeros(shape, dtype=int)
     # Fill borders
     maze[0, :] = maze[-1, :] = 1
     maze[:, 0] = maze[:, -1] = 1
-    minx = minspace + 1
-    maxx = ((width * 2) + 1) - minspace
-    miny = minspace + 1
-    maxy = ((height * 2) + 1) - minspace
+    minx = 0
+    maxx = (width * 2)
+    miny = 0
+    maxy = (height * 2)
     maze = maze_section(maze, minx, maxx, miny, maxy)
     return maze
 
 
 def maze_section(maze, minx, maxx, miny, maxy):
     skip = 0
-    # if match, set wall, otherwise make it random
-    if minx == maxx:
-        wallx = minx
-    else:
-        wallx = rand(minx, maxx)
-    if miny == maxy:
-        wally = miny
-    else:
-        wally = rand(miny, maxy)
+    # check for 1 and 2 space corridors
+    if (maxx - minx) <= (wall + (corr * 2)) or (maxy - miny) <= (wall + (corr * 2)):
+        return maze
+    # Push wall & corr out of min, but leave it in the max as the max is never picked
+    wallx = rand((minx + corr + wall), maxx - wall)
+    wally = rand((miny + corr + wall), maxy - wall)
     # set bits to 1 for the walls
-    maze[wally, (minx - minspace):(maxx + minspace)] = 1
-    maze[(miny - minspace):(maxy + minspace), wallx] = 1
+    maze[wally, (minx + wall):maxx] = 2
+    maze[(miny + wall):maxy, wallx] = 2
     # Make 3 doors at random
     # coin flip to see if a door should be placed, otherwise flag a skip
     if rand(1, 3) == 1:
-        maze = door(maze, minx, wallx, wally, "y")  # N
-        print("North Door Made")
+        print("North")
+        maze = door(maze, "x", wallx, miny, wally)  # N
     else:
         skip = 1
     if rand(1, 3) == 1 or skip == 1:
-        maze = door(maze, wally, maxy, wallx, "x")  # E
-        print("East Door Made")
+        print("East")
+        maze = door(maze, "y", wally, wallx, maxx)  # E
     else:
         skip = 1
     if rand(1, 3) == 1 or skip == 1:
-        maze = door(maze, wallx, maxx, wally, "y")  # S
-        print("South Door Made")
+        print("South Door")
+        maze = door(maze, "x", wallx, wally, maxy)  # S
     else:
         skip = 1
     if skip == 1:
-        maze = door(maze, miny, wally, wallx, "x")  # W
-        print("West Door Made")
+        print("West")
+        maze = door(maze, "y", wally, minx, wallx)  # W
 
-    if wallx - minspace > minx and wally - minspace > miny:
-        maze = maze_section(maze, minx, (wallx - minspace - 1), miny, (wally - minspace))
-    if wallx + minspace < maxx and wally - minspace > miny:
-        maze = maze_section(maze, (wallx + minspace), maxx, miny, (wally - minspace))
-    if wallx - minspace > minx and wally + minspace < maxy:
-        maze = maze_section(maze, minx, (wallx - minspace), (wally + minspace), maxy)
-    if wallx + minspace < maxx and wally + minspace < maxy:
-        maze = maze_section(maze, (wallx + minspace), maxx, (wally + minspace), maxy)
+    maze = maze_section(maze, minx, wallx, miny, wally)
+    maze = maze_section(maze, wallx, maxx, miny, wally)
+    maze = maze_section(maze, minx, wallx, wally, maxy)
+    maze = maze_section(maze, wallx, maxx, wally, maxy)
     return maze
 
 
-def door(maze, mind, maxd, nondoor, dooraxis):
-    if (maxd - mind) <= doorwidth:
-        doormin = mind
-        doormax = maxd
+def door(maze, axis, wallpoint, mind, maxd):
+    door = rand((mind + wall), maxd)
+    if axis == "x":
+        maze[door, wallpoint] = 0
     else:
-        doormin = rand(mind, (maxd - doorwidth))
-        doormax = (doormin + doorwidth)
-    print(dooraxis, doormin, doormax, nondoor)
-    if dooraxis == "x":
-        maze[nondoor, round(doormin):round(doormax)] = 0
-    else:
-        maze[round(doormin):round(doormax), nondoor] = 0
+        maze[wallpoint, door] = 0
     return maze
 
 
-width = 15
-height = 15
-minspace = 2
-doorwidth = 1 # round((minspace / 2), 0)
+width = 5
+height = 5
+corr = 1
+wall = 1
 
 pyplot.figure(figsize=(10, 5))
 pyplot.imshow(make_maze(), cmap=pyplot.cm.binary, interpolation=None)
