60180439b730eac7ad6cf38da05d73b97ddb8ccd
AceHelice <AceHelice@Gmail.com>
2019-03-03 14:52:45 -0800

    Button Removed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 from settings import Settings
 from slime import Slime
-from Button import Button
 import game_functions as gf
 
 
@@ -12,16 +11,14 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Meandering Muck")
 
-    # Make the play_button
-    play_button = Button(ai_settings, screen, "Begin")
     # Make a Slime.
     slime = Slime(ai_settings, screen)
 
     # Start main loop for the game.
     while True:
-        gf.check_events(play_button, slime)
+        gf.check_events(slime)
         slime.update()
-        gf.update_screen(ai_settings, screen, slime, play_button)
+        gf.update_screen(ai_settings, screen, slime)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
